514ed0e2ccbe4d333491bb466f7358ca06163e0f
Teddy Leung <teddy.ph.leung@gmail.com>
2020-01-06 16:49:47 -0800

    Implemented addresses#create.

---
+++ backend/app/controllers/addresses_controller.rb
@@ -0,0 +1,13 @@
+class AddressesController < ApplicationController
+  def create
+    @address = Address.new(
+      address: (Faker::Name.first_name + '.' + Faker::Name.last_name).downcase + '.' + SecureRandom.hex(3)
+    )
+
+    if @address.save
+      render :show
+    else
+      render json: { error: 'Failed to create address' }, status: 400
+    end
+  end
+end
