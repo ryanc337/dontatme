086617f7413df870075f415e9032880d92911059
teddyleung <teddyleung@users.noreply.github.com>
2020-01-07 10:35:55 -0800

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,17 +1,11 @@
 import axios from 'axios';
 
 export const getAddress = async () => {
-  const response = await axios.post('http://localhost:3001/addresses')
+  const response = await axios.post('http://localhost:3001/addresses');
   return response.data;
-}
+};
 
 export const getEmails = async () => {
-  const response = await axios.get('http://localhost:3001/teddyisthegoat')
+  const response = await axios.get('http://localhost:3001/teddyisthegoat');
   return response.data;
-}
-
-
-
-
-
-
+};
