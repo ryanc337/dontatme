4cbae852567cc62e3cc44550f4508cce793a0404
Ryan Cordingley <52723940+ryanc337@users.noreply.github.com>
2020-01-09 17:52:56 -0800

    added comment for moment todo in list item

--- frontend/src/components/emails/EmailListItem.js
+++ frontend/src/components/emails/EmailListItem.js
@@ -8,7 +8,7 @@ const EmailListItem = ({ email, setFocusPanel, setFocusId, focusId }) => {
     setFocusId(id);
     setFocusPanel('email');
   }
-
+//TODO: change moment format so that it will format based on time ago
   return (
     <div className={`EmailListItem ${focusId === id ? 'highlight-email-list-item': 'email-list-item'}`} onClick={() => handleClick(email.id)}>
       <h1>{senderName}</h1>
