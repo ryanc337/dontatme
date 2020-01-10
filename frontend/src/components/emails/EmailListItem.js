27259bd06246a31c1419ffbd4115dba91298e9d7
ryanc337 <cordingley.ryan@gmail.com>
2020-01-09 18:04:17 -0800

    commit before pull

--- frontend/src/components/emails/EmailListItem.js
+++ frontend/src/components/emails/EmailListItem.js
@@ -4,7 +4,9 @@ import moment from 'moment';
 const EmailListItem = ({ email, setFocusPanel, setFocusId, focusId }) => {
   const { from, sent_at, subject, body, has_attachments, id } = email;
   const senderParse = JSON.parse(from);
-  const senderName = senderParse[0]['name'];
+  // const senderName = senderParse[0]['name'];
+  const senderName = senderParse[0].name
+  console.log(senderName)
   const handleClick = (id) => {
     setFocusId(id);
     setFocusPanel('email');
