1ff1829594b32a0e2d0db0f4ef0f0778e8832e73
Teddy Leung <teddy.ph.leung@gmail.com>
2020-01-13 00:39:42 -0800

    Styled header and hero.

--- frontend/src/components/emails/EmailEmpty.js
+++ frontend/src/components/emails/EmailEmpty.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 const EmailEmpty = () => {
   return (
-    <div className="EmailEmpty">
-      Email Empty
-    </div>
+    <div className="email-empty"></div>
   )
 }
 
--- frontend/src/components/landing/Header.js
+++ frontend/src/components/landing/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
 
 const Header = () => {
-  return(
-    <h1>Don't @ me</h1>
+  return (
+    <div className='header'>
+      <div className='logo'><Logo /></div>
+    </div>
   )
 }
 
