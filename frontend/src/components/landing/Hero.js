6ca9e6412ecc94071e5d595148bbbeb07924632f
ryanc337 <cordingley.ryan@gmail.com>
2020-01-13 17:24:54 -0800

    megered from master

--- frontend/src/components/landing/Hero.js
+++ frontend/src/components/landing/Hero.js
@@ -6,19 +6,6 @@ const Hero = ({ address }) => {
     addressRef.current.select();
     document.execCommand('copy');
   };
-<<<<<<< HEAD
-
-  return (
-    <section className="hero">
-      <h1 className="hero__tagline">
-Practice safe emails...
-        <br />
-Use a burner
-      </h1>
-      <div className="hero__cta">
-        <input className="hero__cta-input" ref={addressRef} value={`${address}@dontatme.ca`} readOnly />
-        <button className="hero__cta-btn" onClick={copyToClipboard} type="button">Copy</button>
-=======
   
   return(
     <section className='hero'>
@@ -26,7 +13,6 @@ Use a burner
       <div className='hero__cta'>
         <input className='hero__cta-input' ref={addressRef} value={address+'@dontatme.ca'} readOnly />
         <button className='hero__cta-btn' onClick={copyToClipboard}>Copy</button>
->>>>>>> 3bd48127577a613928f1203070e6b82b2846742d
       </div>
     </section>
   );
