2db361a5f4276fcb38812cfa53822d0d7134013a
ryanc337 <cordingley.ryan@gmail.com>
2020-01-15 13:12:37 -0800

    added icon to chrome extension header

--- chrome/src/components/emails/EmailListHeader.js
+++ chrome/src/components/emails/EmailListHeader.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { ReactComponent as Copy } from '../../assets/copy.svg';
 
-const EmailListHeader = ({ address }) => (
-  <div className="email-list-header">
-    <input className="email-list-header__address-id" value={address+'@dontatme.ca'} readOnly/>
-  </div>
-);
+const EmailListHeader = ({ address }) => {
+  const addressRef = useRef(null);
+  const copyToClipboard = () => {
+    addressRef.current.select();
+    document.execCommand('copy');
+  };
+
+  return (
+    <div className="email-list-header">
+      <input className="email-list-header__address-id" ref={addressRef} value={address+'@dontatme.ca'} readOnly/>
+      <div className="email-list-header__copy" onClick={copyToClipboard}><Copy/></div>
+    </div>
+  );
+};
 
 export default EmailListHeader;
